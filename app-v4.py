1b43565ce2de1d7c58b08654a405197cc8e746dc
arizidddannugraha <arizidan.nugraha@gmail.com>
2026-09-25 00:02:09 +0700

    revoke

--- app-v4.py
+++ app-v4.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
-from statsmodels.tsa.arima.model import ARIMA
+# from statsmodels.tsa.arima.model import ARIMA
 
 # --- PAGE CONFIGURATION ---
 st.set_page_config(
